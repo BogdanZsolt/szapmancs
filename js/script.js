d97432eded81e306ac4da93e7419a697bb89ad6e
BogdanZsolt <bogdan.zsoltika@gmail.com>
2020-12-25 20:16:28 +0100

    update

--- js/script.js
+++ js/script.js
@@ -8,26 +8,15 @@ const menuItems = document.querySelector('.navbar__items');
 const expandBtn = document.querySelectorAll('.expand-btn');
 const navBar = document.querySelector('.navbar');
 const sideBar = document.querySelector('.faq__navbar');
-// const sideBarTop = sideBar.offsetTop;
-// const sideBarHight = sideBar.offsetHeight;
-// const sideBarPos = sideBarTop + sideBarHight;
-const faqFooter = document.querySelector('.faq__footer');
-// const maxScroll = faqFooter.offsetTop - 30;
-const faqAccordion = document.querySelectorAll('.faq__accordion');
-var faqNavLinks = document.querySelectorAll('.faq__nav--link');
+const faqAccordionQuestion = document.querySelectorAll(
+  '.faq__accordion--question'
+);
+// var faqNavLinks = document.querySelectorAll('.faq__nav--link');
 
 // Sticky NavBar
 window.addEventListener('scroll', () => {
   navBar.classList.toggle('sticky', window.scrollY > 0);
 });
-
-// Sticky Sidebar
-// window.addEventListener('scroll', () => {
-//   sideBar.classList.toggle(
-//     'stickySide',
-//     window.scrollY > 0 && sideBarPos + window.scrollY < maxScroll
-//   );
-// });
 
 // hamburger toggle
 menuBtn.addEventListener('click', () => {
@@ -42,19 +31,31 @@ expandBtn.forEach((btn) => {
   });
 });
 
-faqAccordion.forEach((panel) => {
+faqAccordionQuestion.forEach((panel) => {
   panel.addEventListener('click', () => {
     panel.classList.toggle('active');
     panel.nextElementSibling.classList.toggle('active');
   });
 });
 
-faqNavLinks.forEach((links) => {
-  links.addEventListener('click', () => {
-    var activeLink = document.querySelectorAll('.active--link');
-    activeLink.forEach((alink) => {
-      alink.classList.remove('active--link');
+window.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        document
+          .querySelector(`nav li a[href="#${id}"]`)
+          .parentElement.classList.add('active');
+      } else {
+        document
+          .querySelector(`nav li a[href="#${id}"]`)
+          .parentElement.classList.remove('active');
+      }
     });
-    links.classList.toggle('active--link');
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('section[id]').forEach((section) => {
+    observer.observe(section);
   });
 });
